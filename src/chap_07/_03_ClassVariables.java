2af18ba8cd99b5410e5e6b4c5d82291422e891ca
chorongs <charmblues@gmail.com>
2023-03-18 00:24:08 +0900

    클래스 변수

---
+++ src/chap_07/_03_ClassVariables.java
@@ -0,0 +1,28 @@
+package chap_07;
+
+public class _03_ClassVariables {
+    public static void main(String[] args) {
+        CellPhone p1 = new CellPhone();
+        p1.cellphone = "아이폰14 PRO MAX";
+        System.out.println(p1.cellphone);
+
+        CellPhone p2 = new CellPhone();
+        p2.cellphone = "베가레이서";
+        System.out.println(p2.cellphone);
+
+
+        // 휴대폰 결제 가능 여부
+        System.out.println("애플 페이 미지원 국가");
+        System.out.println(p1.cellphone + " 애플 페이 : " + p1.canMobilePay);
+        System.out.println(p2.cellphone + " 삼성 페이 : " + p2.canMobilePay);
+        System.out.println("휴대폰 결제 기능 : " + CellPhone.canMobilePay);
+
+        // 모든 기종 휴대폰 결제 도입 완료
+        CellPhone.canMobilePay = true;
+
+        System.out.println("애플 페이 지원 국가");
+        System.out.println(p1.cellphone + " 애플 페이 : " + p1.canMobilePay);
+        System.out.println(p2.cellphone + " 삼성 페이 : " + p2.canMobilePay);
+        System.out.println("휴대폰 결제 기능 : " + CellPhone.canMobilePay);
+    }
+}
