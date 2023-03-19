95eda963da1815b9cf244aa7d6abb3f4cae43a08
chorongs <charmblues@gmail.com>
2023-03-20 02:42:35 +0900

    Super

--- src/chap_07/camera/SpeedCam.java
+++ src/chap_07/camera/SpeedCam.java
@@ -8,6 +8,13 @@ public class SpeedCam extends Camera{ // 자식 클래스
         this.name = "과속단속 카메라";
     }
 
+    public void takePicture() {
+        //사진 촬영
+        super.takePicture();
+        checkSpeed();
+        recognizeLicensePlate();
+    }
+
 
     public void checkSpeed() {
         // 속도 체크
